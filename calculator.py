42ac3616ad38a78673dc2164aa93060c5c922d84
shuroq <Cherry@Shuroqs-MacBook-Pro.local>
2016-02-22 16:22:34 -0500

    week 6

--- calculator.py
+++ calculator.py
@@ -1,17 +1,27 @@
 def add( first, second):
     # TODO:
     # there's an error in this code, fix it
-    return first plus second
-
+    result = first + second
+    return result
 def subtract( first, second):
     # TODO:
     # fill in code here that will return the difference between first and second
-
+    result = first - second
+    return result
 def multiply( first, second):
     # TODO:
     # fill in code here that will return the product of first and second
-
+     result = first * second
+     return result 
 def divide( first, second):
+    if second == 0:
+        print ( "I'm sorry I can't divide by zero ")
+        return 0
+    else:
+     result = first / second
+     return result
+     
+    
     # TODO:
     # fill in code here that:
     #   1. checks the second number to see if it is zero
